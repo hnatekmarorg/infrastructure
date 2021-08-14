8301f4a5f91723beef629eea416fbceb22bff428
Martin Hnátek <martinhnatek@sanezoo.com>
2021-08-14 12:02:30 +0200

    Basic infrastructer (gitlab)

---
+++ README.md
@@ -0,0 +1,6 @@
+# Usage
+Create env variables with info about your cluster:
+
+- `PROXMOX_HOST` - url of proxmox for example `https://<host>:8006/api2/json` (optional)
+- Authentication info for cluster as described [here](https://github.com/Telmate/terraform-provider-proxmox/blob/v2.7.4/docs/index.md)
+
---
+++ base.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    proxmox = {
+      source = "telmate/proxmox"
+      version = "2.7.4"
+    }
+  }
+}
+
+variable"PROXMOX_HOST" {
+  default = "https://172.16.100.3:8006/api2/json"
+}
+
+provider "proxmox" {
+  pm_api_url = var.PROXMOX_HOST
+  pm_tls_insecure = true
+}
+
+module "gitlab" {
+  source = "./modules/gitlab"
+}
