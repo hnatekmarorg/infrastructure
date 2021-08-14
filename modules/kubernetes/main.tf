6b11d3a268a8f1c6fe36bbc27140702366df71b5
Martin Hnátek <martinhnatek@sanezoo.com>
2021-08-14 23:30:05 +0200

    Kube install

--- modules/kubernetes/main.tf
+++ modules/kubernetes/main.tf
@@ -6,10 +6,10 @@ terraform {
   }
 }
 
-resource "proxmox_vm_qemu" "gitlab" {
-  name = "gitlab-server"
-  desc = "Server with gitlab on it"
-  target_node = "ps0"
+resource "proxmox_vm_qemu" "k8s-0" {
+  name = "k8s-0"
+  desc = "Master server for k8s"
+  target_node = "ps2"
   agent = 1
   clone = "debian-base"
   full_clone = true
@@ -21,7 +21,11 @@ resource "proxmox_vm_qemu" "gitlab" {
   ipconfig0 = "ip=172.16.100.99/24,gw=172.16.100.1"
   sshkeys = file("~/.ssh/id_ed25519.pub")
   provisioner "local-exec" {
-    command = "k3sup install --ip 172.16.100.99 --user root --k3s-extra-args '--no-deploy traefik' --local-path ~/.kube/config"
+    command = "ansible-playbook -i inventory.ini init.yaml"
+    working_dir = "./modules/kubernetes"
+  }
+  provisioner "local-exec" {
+    command = "k3sup install --ip 172.16.100.99 --ssh-key ~/.ssh/id_ed25519 --user root --k3s-extra-args '--no-deploy traefik' --local-path ~/.kube/config"
   }
 }
 
